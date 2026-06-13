15b0021a71cb39d335f2be52b6a084cc89ff13f5
richa <riclarobr04@gmail.com>
2026-06-13 18:57:36 -0500

    chore: cleanup old dln components, theme package and demo app

--- projects/deline-core/src/public-api.ts
+++ projects/deline-core/src/public-api.ts
@@ -1,13 +1,5 @@
-/*
- * Public API Surface of deline-core
- */
+export * from './lib/models';
 
-export * from './lib/dln-button/dln-button';
-export * from './lib/dln-select/dln-select';
-export * from './lib/dln-input/dln-input';
-export * from './lib/dln-textarea/dln-textarea';
-export * from './lib/dln-checkbox/dln-checkbox';
-export * from './lib/dln-radio-button/dln-radio-button';
-export * from './lib/dln-radio-group/dln-radio-group';
-export * from './lib/dln-card/dln-card';
-export * from './lib/dln-slide-toggle/dln-slide-toggle';
+export * from './lib/components';
+
+export * from './lib/services';
